582a7aeae2e79cb0a3b2fe82cec3b46aec7eee03
gabrielstrzal <gabrielstrzal@gmail.com>
2017-09-03 18:42:04 -0300

    env setup

---
+++ desktop/src/com/gstrzal/insects/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gstrzal.insects.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gstrzal.insects.Insects;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Insects(), config);
+	}
+}
