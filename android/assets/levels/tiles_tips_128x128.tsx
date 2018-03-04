618abec204a87b4cfea74e4df7c402dc8c74b8c6
Gabriel Strzal <gabrielstrzal@gmail.com>
2018-03-04 12:07:49 -0300

    made changes to level design and art

---
+++ android/assets/levels/tiles_tips_128x128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_tips_128x128" tilewidth="128" tileheight="128" tilecount="12" columns="6">
+ <image source="tiles_tips_128x128.png" width="768" height="256"/>
+</tileset>
